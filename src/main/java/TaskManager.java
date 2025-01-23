6723c1c4527de33dfad6d136b024bbe250e1baa8
Jun <leejunheng2001@gmail.com>
2025-01-24 07:08:34 +0800

    add support for todo, event and deadline task

--- src/main/java/TaskManager.java
+++ src/main/java/TaskManager.java
@@ -10,52 +10,95 @@ public class TaskManager {
     }
 
     protected void executeTask(String input) {
-        String[] words = input.split(" ");
-        String command = words[0];
+        String command = input.split(" ")[0];
         try {
             switch (command) {
                 case "list":
                     listTasks();
                     break;
                 case "mark":
-                    markTask(Integer.parseInt(words[1]));
+                    markTask(input);
                     break;
                 case "unmark":
-                    unmarkTask(Integer.parseInt(words[1]));
+                    unmarkTask(input);
+                    break;
+                case "todo":
+                    addTodoTask(input);
+                    break;
+                case "deadline":
+                    addDeadlineTask(input);
+                    break;
+                case "event":
+                    addEventTask(input);
                     break;
                 default:
-                    addTask(input);
-                    break;
+                    System.out.println("I'm sorry, but I don't know what that means :-(");
             }
-        } catch (NumberFormatException e) {
-            System.out.println("Please provide a valid task number!");
-        } catch (IndexOutOfBoundsException e) {
-            System.out.println("Invalid task number!");
         } catch (Exception e) {
-            System.out.println("An error occurred: " + e.getMessage());
+            System.out.println(e.getMessage());
         }
     }
 
-    private void markTask(int index){
-        if (!isValidIndex(index)) {
-            throw new IndexOutOfBoundsException("Invalid task number!");
-        }
-        Task task = tasks.get(index - 1);
-        task.mark();
-        System.out.println("Nice! I've marked this task as done:\n  " + task);
+    private void addTask(Task task) {
+        tasks.add(task);
+        System.out.println("Got it. I've added this task:");
+        System.out.println("  " + task);
+        System.out.println("Now you have " + tasks.size() + " tasks in the list.");
     }
 
-    private void unmarkTask(int index) {
-        if (!isValidIndex(index)) {
-            throw new IndexOutOfBoundsException("Invalid task number!");
+    private void addEventTask(String input) {
+        try {
+            String description = input.split(" ", 2)[1];
+            String[] event = description.split(" /from | /to ");
+            Task task = new Event(event[0], event[1], event[2]);
+            addTask(task);
+        } catch (IndexOutOfBoundsException e) {
+            throw new IndexOutOfBoundsException("Please provide a valid event description!");
         }
-        Task task = tasks.get(index - 1);
+    }
+
+    private void addDeadlineTask(String input) {
+        try {
+            String description = input.split(" ", 2)[1];
+            String[] deadline = description.split(" /by ");
+            Task task = new Deadline(deadline[0], deadline[1]);
+            addTask(task);
+        } catch (IndexOutOfBoundsException e) {
+            throw new IndexOutOfBoundsException("Please provide a valid deadline description!");
+        }
+    }
+
+    private void addTodoTask(String input) {
+        try {
+            String description = input.split(" ", 2)[1];
+            Task task = new Todo(description);
+            addTask(task);
+        } catch (IndexOutOfBoundsException e) {
+            throw new IndexOutOfBoundsException("Please provide a valid todo description!");
+        }
+    }
+
+    private Task getTask(String input) {
+        try {
+            int index = Integer.parseInt(input.split(" ")[1]);
+            return tasks.get(index - 1);
+        } catch (IndexOutOfBoundsException e) {
+            throw new IndexOutOfBoundsException("Please provide a valid task number!");
+        } catch (NumberFormatException e) {
+            throw new NumberFormatException("Task number must be a number!");
+        }
+    }
+
+    private void unmarkTask(String input){
+        Task task = getTask(input);
         task.unmark();
         System.out.println("OK, I've marked this task as not done yet:\n  " + task);
     }
 
-    private boolean isValidIndex(int index) {
-        return index > 0 && index <= tasks.size();
+    private void markTask(String input){
+        Task task = getTask(input);
+        task.mark();
+        System.out.println("Nice! I've marked this task as done:\n  " + task);
     }
 
     private void listTasks() {
@@ -64,12 +107,6 @@ public class TaskManager {
                 .reduce((x, y) -> x + "\n" + y)
                 .orElse("No tasks added yet!");
 
-        System.out.println(list);
-    }
-
-    private void addTask(String description) {
-        Task task = new Task(description);
-        tasks.add(task);
-        System.out.println("added: " + task);
+        System.out.println("Here are the tasks in your list:\n" + list);
     }
 }
