fceef0a2ad585fd4e598ba7594cf6d7cf29b7faf
Jun <leejunheng2001@gmail.com>
2025-01-24 02:27:46 +0800

    Add support to mark and unmark in Task class

--- src/main/java/Task.java
+++ src/main/java/Task.java
@@ -1,13 +1,27 @@
 public class Task {
 
     private final String description;
+    private boolean isDone;
 
     public Task(String description) {
         this.description = description;
+        this.isDone = false;
+    }
+
+    protected void mark() {
+        this.isDone = true;
+    }
+
+    protected void unmark() {
+        this.isDone = false;
+    }
+
+    private String getStatusIcon() {
+        return (isDone ? "X" : " "); // mark done task with X
     }
 
     @Override
     public String toString() {
-        return description;
+        return "[" + getStatusIcon() + "] " + description;
     }
 }
