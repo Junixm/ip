679e7813ccef0217f2fcebdbfd36ea528b2de86f
Jun <leejunheng2001@gmail.com>
2025-01-24 01:30:31 +0800

    Added support for addTask

--- src/main/java/Treky.java
+++ src/main/java/Treky.java
@@ -1,8 +1,12 @@
 import java.util.Scanner;
+import java.util.ArrayList;
+import java.util.stream.IntStream;
 
 public class Treky {
 
     private final static String lineSpacer = "____________________________________________________________";
+    private static ArrayList<Task> tasks = new ArrayList<>();
+
     public static void main(String[] args) {
 
         try (Scanner sc = new Scanner(System.in)) {
@@ -10,16 +14,35 @@ public class Treky {
             while (true) {
                 String input = sc.nextLine();
                 System.out.println(lineSpacer);
-                if (input.equals("bye")) {
-                    goodbye();
-                    break;
-                } else {
-                    echo(input);
+                switch (input) {
+                    case "bye":
+                        goodbye();
+                        return;
+                    default:
+                        addTask(input);
+                        break;
                 }
             }
         }
 
     }
+
+    private static void addTask(String input) {
+        Task task = new Task(input);
+        tasks.add(task);
+        echo("added: " + task);
+    }
+
+    private static void echo(String input) {
+        System.out.println(input);
+        System.out.println(lineSpacer);
+    }
+
+    private static void goodbye() {
+        System.out.println("Bye. Hope to see you again soon!");
+        System.out.println(lineSpacer);
+    }
+
     private static void hello() {
         String logo = """
               _____        _       \s
@@ -32,16 +55,6 @@ public class Treky {
         System.out.println(lineSpacer);
         System.out.println(logo);
         System.out.println("Hello! I'm Treky\nWhat can I do for you?");
-        System.out.println(lineSpacer);
-    }
-
-    private static void goodbye() {
-        System.out.println("Bye. Hope to see you again soon!");
-        System.out.println(lineSpacer);
-    }
-
-    private static void echo(String input) {
-        System.out.println(input);
         System.out.println(lineSpacer);
     }
 }
