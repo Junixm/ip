15e024183c5d6440f94c549e12cfe080107c17d0
Jun <leejunheng2001@gmail.com>
2025-01-29 06:47:46 +0800

    Refactor Treky.java: Add Storage class

--- src/main/java/Treky.java
+++ src/main/java/Treky.java
@@ -2,22 +2,29 @@ import java.util.Scanner;
 
 public class Treky {
     static final String LINE_SEPARATOR = "____________________________________________________________";
+    private CommandHandler commandHandler;
 
-    public static void main(String[] args) {
-        TaskManager taskManager = new TaskManager();
-        CommandHandler commandHandler = new CommandHandler(taskManager);
+    public Treky(String filePath) {
+        try {
+            Storage storage = new Storage(filePath);
+            TaskManager taskManager = new TaskManager(storage.loadTasks());
+            this.commandHandler = new CommandHandler(taskManager, storage);
+        } catch (TrekyException e) {
+            System.out.println(e.getMessage());
+            System.exit(1);
+        }
+    }
+
+    public void run() {
         Scanner sc = new Scanner(System.in);
-
         hello();
-
         while (true) {
             System.out.print("> ");
-
             if (!sc.hasNextLine()) {
+                goodbye();
                 break;
             }
             String input = sc.nextLine();
-
             System.out.println(LINE_SEPARATOR);
 
             if (input.equalsIgnoreCase("bye")) {
@@ -36,14 +43,15 @@ public class Treky {
 
             System.out.println(LINE_SEPARATOR);
         }
+        sc.close();
     }
 
-    private static void goodbye() {
+    private void goodbye() {
         System.out.println("Goodbye! Have a great day!");
         System.out.println(LINE_SEPARATOR);
     }
 
-    private static void hello() {
+    private void hello() {
         String logo = """
               _____        _       \s
              |_   _| _ ___| |___  _\s
@@ -56,5 +64,9 @@ public class Treky {
         System.out.println(logo);
         System.out.println("Hello! I'm Treky\nWhat can I do for you?");
         System.out.println(LINE_SEPARATOR);
+    }
+
+    public static void main(String[] args) {
+        new Treky("./data/tasks.txt").run();
     }
 }
