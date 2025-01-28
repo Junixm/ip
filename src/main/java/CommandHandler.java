7e5b123ee8a961885c9d6029a9f913b2770593bd
Jun <leejunheng2001@gmail.com>
2025-01-29 02:37:02 +0800

    Add CommandHandler.java

---
+++ src/main/java/CommandHandler.java
@@ -0,0 +1,103 @@
+public class CommandHandler {
+    private final TaskManager taskManager;
+
+    public CommandHandler(TaskManager taskManager) {
+        this.taskManager = taskManager;
+    }
+
+    public void executeCommand(String command, String description) {
+        try {
+            switch (command.toLowerCase()) {
+            case "todo":
+                addTask(description);
+                break;
+            case "deadline":
+                addDeadlineTask(description);
+                break;
+            case "event":
+                addEventTask(description);
+                break;
+            case "list":
+                taskManager.listTasks();
+                break;
+            case "mark":
+                setMarkTask(description, true);
+                break;
+            case "unmark":
+                setMarkTask(description, false);
+                break;
+            case "delete":
+                deleteTask(description);
+                break;
+            default:
+                throw new IllegalArgumentException("I'm sorry, but I don't know what that means :-(");
+            }
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    private void addTask(String description) throws TrekyException {
+        if (description.isEmpty()) {
+            throw new TrekyException("The description of a todo cannot be empty.");
+        }
+        Task task = new Todo(description);
+        taskManager.addTask(task);
+    }
+
+    private void addDeadlineTask(String description) throws TrekyException {
+        if (description.isEmpty()) {
+            throw new TrekyException("The description of a deadline cannot be empty.");
+        }
+        String[] parts = description.split(" /by ");
+        if (parts.length < 2) {
+            throw new TrekyException("Deadline must include '/by' followed by date/time.");
+        }
+        Task task = new Deadline(parts[0], parts[1]);
+        taskManager.addTask(task);
+    }
+
+    private void addEventTask(String description) throws TrekyException {
+        if (description.isEmpty()) {
+            throw new TrekyException("The description of an event cannot be empty.");
+        }
+        String[] parts = description.split(" /from | /to ");
+        if (parts.length < 3) {
+            throw new TrekyException("Event must include '/from' and '/to' followed by dates/times.");
+        }
+        Task task = new Event(parts[0], parts[1], parts[2]);
+        taskManager.addTask(task);
+    }
+
+    private void setMarkTask(String description, boolean isDone) throws TrekyException {
+        if (description.isEmpty()) {
+            throw new TrekyException("Please provide a task number.");
+        }
+        try {
+            int index = Integer.parseInt(description);
+            taskManager.markTask(index - 1, isDone);
+        } catch (NumberFormatException e) {
+            throw new TrekyException("Task number must be a number!");
+        } catch (IndexOutOfBoundsException e) {
+            throw new TrekyException("Please provide a valid task number!");
+        } catch (Exception e) {
+            throw new TrekyException(e.getMessage());
+        }
+    }
+
+    private void deleteTask(String description) throws TrekyException {
+        if (description.isEmpty()) {
+            throw new TrekyException("Please provide a task number.");
+        }
+        try {
+            int index = Integer.parseInt(description);
+            taskManager.deleteTask(index - 1);
+        } catch (NumberFormatException e) {
+            throw new TrekyException("Task number must be a number!");
+        } catch (IndexOutOfBoundsException e) {
+            throw new TrekyException("Please provide a valid task number!");
+        } catch (Exception e) {
+            throw new TrekyException(e.getMessage());
+        }
+    }
+}
