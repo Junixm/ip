efcbe74efdfcf6de76bdd694d93c9d1fa8f77afa
Jun <leejunheng2001@gmail.com>
2025-01-29 11:02:44 +0800

    Refactor code

--- src/main/java/Task.java
+++ src/main/java/Task.java
@@ -2,11 +2,6 @@ public class Task {
     private final String description;
     private boolean isDone;
 
-    public Task(String description) {
-        this.description = description;
-        this.isDone = false;
-    }
-
     public Task(String description, boolean isDone) {
         this.description = description;
         this.isDone = isDone;
