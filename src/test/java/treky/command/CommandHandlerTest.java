9641502cd5a85f563930dba269b7a149090dedac
Jun <leejunheng2001@gmail.com>
2025-02-01 06:32:38 +0800

    Add JUnit test for CommandHandler

---
+++ src/test/java/treky/command/CommandHandlerTest.java
@@ -0,0 +1,56 @@
+package treky.command;
+
+import org.junit.jupiter.api.Test;
+import treky.TrekyException;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.fail;
+
+public class CommandHandlerTest {
+
+    @Test
+    public void parse_emptyInput_exceptionThrown() {
+        CommandHandler commandHandler = new CommandHandler(null, null);
+
+        try {
+            commandHandler.parse("");
+            fail();
+        } catch (TrekyException e) {
+            assertEquals("You didn't enter anything!\nHow can I help you?", e.getMessage());
+        }
+    }
+
+    @Test
+    public void parse_randomInput_exceptionThrown() {
+        CommandHandler commandHandler = new CommandHandler(null, null);
+
+        try {
+            commandHandler.parse("random");
+            fail();
+        } catch (TrekyException e) {
+            assertEquals("I'm sorry, but I don't know what that means :-(", e.getMessage());
+        }
+    }
+
+    @Test
+    public void execute_defaultCase_exceptionThrown() {
+        CommandHandler commandHandler = new CommandHandler(null, null);
+
+        try {
+            commandHandler.execute("random", "");
+            fail();
+        } catch (TrekyException e) {
+            assertEquals("I'm sorry, but I don't know what that means :-(", e.getMessage());
+        }
+    }
+
+    @Test
+    public void execute_byeCommand_success() {
+        CommandHandler commandHandler = new CommandHandler(null, null);
+        try {
+            assertEquals("bye", commandHandler.execute("bye", ""));
+        } catch (TrekyException e) {
+            fail();
+        }
+    }
+}
