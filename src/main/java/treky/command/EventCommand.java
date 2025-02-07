9f2830e8ae3e5237048ef3371795d1fa9f5f65ef
Jun <leejunheng2001@gmail.com>
2025-02-08 02:13:23 +0800

    Add featured command classes

--- src/main/java/treky/command/EventCommand.java
+++ src/main/java/treky/command/EventCommand.java
@@ -5,16 +5,16 @@ import java.time.LocalDate;
 
 import treky.task.TaskList;
 import treky.task.Task;
-import treky.task.Deadline;
+import treky.task.Event;
 import treky.exception.TrekyException;
 
 /**
- * Adds a deadline task to the task list.
+ * Adds an event task to the task list.
  */
-public class DeadlineCommand implements Executable {
+public class EventCommand implements Executable{
     private final Task task;
     private final TaskList taskList;
-    private static final String FORMAT_MESSAGE = "Format: deadline <description> /by <yyyy-MM-dd>";
+    private static final String FORMAT_MESSAGE = "Format: event <description> /from <yyyy-MM-dd> /to <yyyy-MM-dd>";
     private static final String SUCCESS_MESSAGE = """
             Got it. I've added this task:
               %s
@@ -22,24 +22,26 @@ public class DeadlineCommand implements Executable {
 
     /**
      * Constructs a DeadlineCommand object with the specified description and TaskList.
-     * Description must contain the date of the deadline task in this format: "description /by yyyy-MM-dd".
+     * Description must contain the date of the deadline task in this format:
+     * "description /from yyyy-MM-dd /to yyyy-MM-dd".
      *
      * @param description The description with date of the deadline task.
      * @param taskList The TaskList object to manage tasks.
      * @throws TrekyException If the description or date is empty or the date is in an invalid format.
      */
-    public DeadlineCommand(String description, TaskList taskList) throws TrekyException {
-        String[] parts = description.split("/by");
-        if (parts.length != 2) {
+    public EventCommand(String description, TaskList taskList) throws TrekyException {
+        String[] parts = description.split("/from|/to");
+        if (parts.length != 3) {
             throw new TrekyException(FORMAT_MESSAGE);
         }
         String trimDescription = parts[0].trim();
-        String by = parts[1].trim();
-        if (trimDescription.isEmpty() || by.isEmpty()) {
+        String from = parts[1].trim();
+        String to = parts[2].trim();
+        if (trimDescription.isEmpty() || from.isEmpty() || to.isEmpty()) {
             throw new TrekyException(FORMAT_MESSAGE);
         }
         try {
-            this.task = new Deadline(trimDescription, LocalDate.parse(by));
+            this.task = new Event(trimDescription, LocalDate.parse(from), LocalDate.parse(to));
             this.taskList = taskList;
         } catch (DateTimeParseException e) {
             throw new TrekyException(FORMAT_MESSAGE);
