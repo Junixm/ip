38f6d4babfc1bdef2ecc3da42f5d8cd7bec342ca
Jun <leejunheng2001@gmail.com>
2025-02-09 01:10:56 +0800

    Add gui function

--- src/main/java/treky/Treky.java
+++ src/main/java/treky/Treky.java
@@ -1,7 +1,8 @@
 package treky;
 
+import javafx.scene.web.HTMLEditorSkin;
 import treky.task.TaskList;
-import treky.ui.Ui;
+import treky.ui.TextUi;
 import treky.command.CommandHandler;
 import treky.storage.Storage;
 import treky.exception.TrekyException;
@@ -9,10 +10,13 @@ import treky.exception.TrekyFatalException;
 
 public class Treky {
     // Adapted from https://github.com/se-edu/addressbook-level2/blob/master/src/seedu/addressbook/Main.java
-    private Ui ui;
+    private static final String WELCOME_MESSAGE = "Hello! I'm Treky\nWhat can I do for you?";
+    private static final String GOODBYE_MESSAGE = "Goodbye! Have a great day!";
+    private TextUi textUi;
     private Storage storage;
     private TaskList taskList;
     private CommandHandler commandHandler;
+    private static boolean isExit = false;
 
     public static void main(String[] args) {
         new Treky().run();
@@ -21,33 +25,59 @@ public class Treky {
     /** Constructs a Treky object.*/
     public Treky() {
         try {
-            this.ui = new Ui();
+            this.textUi = new TextUi();
             this.storage = new Storage();
             this.taskList = storage.load();
             this.commandHandler = new CommandHandler(taskList);
-            ui.showWelcome();
+            textUi.showLogo();
         } catch (TrekyFatalException e) {
-            ui.showError(e.getMessage());
+            textUi.showError(e.getMessage());
             System.exit(1);
         }
     }
 
    private void run() {
-       boolean isExit = false;
+       textUi.showWelcome();
        while (!isExit) {
            try {
-               String input = ui.readInput();
+               String input = textUi.readInput();
                String result = commandHandler.parse(input);
-               ui.showResult(result);
+               textUi.showResult(result);
                isExit = commandHandler.getExit();
                storage.save(taskList);
            } catch (TrekyException e) {
-               ui.showError(e.getMessage());
+               textUi.showError(e.getMessage());
            } catch (TrekyFatalException e) {
-               ui.showError(e.getMessage());
+               textUi.showError(e.getMessage());
                System.exit(1);
            }
        }
-       ui.showGoodbye();
+       textUi.showGoodbye();
+       System.exit(0);
+   }
+
+   public String getWelcomeMessage() {
+       return WELCOME_MESSAGE;
+   }
+
+   public String getResponse(String input) {
+       try {
+           String result = commandHandler.parse(input);
+           isExit = commandHandler.getExit();
+           if (isExit) {
+               return GOODBYE_MESSAGE;
+           }
+           storage.save(taskList);
+           return result;
+       } catch (TrekyException e) {
+           return e.getMessage();
+       } catch (TrekyFatalException e) {
+           isExit = true;
+           return e.getMessage();
+       }
+   }
+
+   public boolean getExit() {
+       return isExit;
    }
 }
